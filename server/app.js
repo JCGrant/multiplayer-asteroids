10bb9bc12a5a52dff6a59b210e464e7885069b02
JCGrant <jamescolin.grant@gmail.com>
2015-12-26 21:25:57 +0000

    Moved code into seperate files.

--- server/app.js
+++ server/app.js
@@ -3,24 +3,18 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var routes = require('./routes');
+var setupSocket = require('./sockets');
 
 port = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use('/', routes);
 app.use('/static', express.static(__dirname + '/../public'));
 
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
-io.on('connection', function(socket) {
-  console.log('A user connected:', socket.id);
-  socket.on('disconnect', function() {
-    console.log('A user disconnected:', socket.id);
-  });
-});
+setupSocket(io);
 
 http.listen(port, function() {
   console.log('listening on http://127.0.0.1:%d', port);
---
+++ server/sockets.js
@@ -0,0 +1,10 @@
+function setupSocket(io) {
+  io.on('connection', function(socket) {
+    console.log('A user connected:', socket.id);
+    socket.on('disconnect', function() {
+      console.log('A user disconnected:', socket.id);
+    });
+  });
+}
+
+module.exports = setupSocket;
